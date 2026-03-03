1e506e49a06a4c28f909aa67429f63067c02ecda
warriors44 <shinya.tsc7@gmail.com>
2026-03-03 21:02:56 +0900

    Add key bindings for history search in zsh configuration: map up and down arrow keys to history search functions for improved navigation.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -52,6 +52,8 @@ zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
 bindkey "^P" history-beginning-search-backward-end
 bindkey "^N" history-beginning-search-forward-end
+bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
 
 # enhancd
 #source ~/.enhancd/init.sh
